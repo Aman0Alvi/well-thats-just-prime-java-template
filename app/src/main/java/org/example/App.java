c7425b943615d496e82452c510bf2e8b98bb1f0a
Aman0Alvi <aman0alvi@gmail.com>
2025-02-02 23:30:02 -0600

    Update App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,12 @@
 package org.example;
 
 public class App {
-  public static void main(String[] args) {
-    // Test your Factorizer class using a driver program here!
-  }
+    public static void main(String[] args) {
+        Factorizer factorizer = new Factorizer();
+
+        System.out.println(factorizer.primeFactors(100)); // [2, 2, 5, 5]
+        System.out.println(factorizer.isPrime(17)); // true
+        System.out.println(factorizer.isComposite(24)); // true
+        System.out.println(factorizer.reduce(12, 26)); // "6/13"
+    }
 }
