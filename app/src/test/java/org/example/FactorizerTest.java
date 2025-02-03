3cb0e2745ebbf900257fa588d1180ebc8f72ece2
Aman0Alvi <aman0alvi@gmail.com>
2025-02-02 23:30:31 -0600

    Update FactorizerTest.java

--- app/src/test/java/org/example/FactorizerTest.java
+++ app/src/test/java/org/example/FactorizerTest.java
@@ -5,9 +5,29 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 
 class FactorizerTest {
-  @Test
-  void itReturnsAnEmptyListForZero() {
-    Factorizer factorizer = new Factorizer();
-    assertEquals(factorizer.primeFactors(0), new ArrayList());
-  }
+    @Test
+    void itReturnsAnEmptyListForZero() {
+        Factorizer factorizer = new Factorizer();
+        assertEquals(new ArrayList<Integer>(), factorizer.primeFactors(0));
+    }
+
+    @Test
+    void itReturnsAnEmptyListForOne() {
+        Factorizer factorizer = new Factorizer();
+        assertEquals(new ArrayList<Integer>(), factorizer.primeFactors(1));
+    }
+
+    @Test
+    void itReturnsCorrectFactorsForPrimeNumbers() {
+        Factorizer factorizer = new Factorizer();
+        assertEquals(Arrays.asList(17), factorizer.primeFactors(17));
+    }
+
+    @Test
+    void itReturnsCorrectFactorsForCompositeNumbers() {
+        Factorizer factorizer = new Factorizer();
+        assertEquals(Arrays.asList(2, 2, 5, 5), factorizer.primeFactors(100));
+        assertEquals(Arrays.asList(3, 3), factorizer.primeFactors(9));
+        assertEquals(Arrays.asList(2, 5), factorizer.primeFactors(10));
+    }
 }
